a6e3bc19ccc0cfbaaa5ab9b0ed90229117eb0831
Yukta-Yadav02 <yuktay.bca2023@ssism.org>
2026-02-12 11:03:39 +0530

    fix: updated CORS for production and local

--- server/index.js
+++ server/index.js
@@ -22,17 +22,14 @@ const PORT = process.env.PORT || 4000;
 /* ================= CORS SETUP ================= */
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "x-request-id",
-      "x-requested-with"
+    origin: [
+      "http://localhost:5173",
+      "https://smart-society-management.vercel.app"
     ],
-  }),
+    credentials: true,
+  })
 );
+
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
